8916f4e5dd431ce6bfc7ab2b6e13e72963cc6f73
Mauro Soto <Drako30976@users.noreply.github.com>
2023-12-01 13:42:50 -0300

    feat: almacenando archivos raiz en cache

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
-if("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./sw.js")
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js")
     .then((registration) => {
-        console.log("service worker registrado");
+      console.log("service worker registrado");
     })
     .catch((error) => {
-        console.log("service worker no registrado");
+      console.log("service worker no registrado");
     });
 }
 
+self.addEventListener("install", (e) => {
+  caches.open("hero-colection").then((cache) => {
+    cache.addAll(["./", "./js/app.js", "./stylo.css", "./img"]);
+  });
+});
 
 self.addEventListener("fetch", (e)=>{
     console.log("pedido de url", e.request.url)
+    caches.has("hero-colection").then(res=>{
+        console.log("cache: ", res)
+    })
 })
